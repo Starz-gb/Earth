93ef9d0e1fbda3f6369595350f42cfd3bc515c5e
Starz-gb <106209294+Starz-gb@users.noreply.github.com>
2022-06-25 10:09:21 -0500

    Create cheats.cpp

---
+++ Cheats/cheats.cpp
@@ -0,0 +1,9 @@
+/* To use cheats
+*  Made by my friend in my discord server potatopc0110
+*/
+#include "cheats.h"
+public static void cheats{
+  if("Plugin Loader Enabled" == true){
+    ///Soon I'm to busy xD///
+  }
+}
